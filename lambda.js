d8a0b4b78280007cf1da04acaa2c373a7d5e0030
Brenn Hill <brenn.hill@previ.se>
2018-11-04 16:20:42 +0700

    Refactor

---
+++ lambda.js
@@ -0,0 +1,19 @@
+/**
+ * This is a lambda handler that also accepts pings to stay warm.
+ * If you are not going to use this microservice as a lambda, you can safely delete it.
+ * @param event
+ * @param context
+ * @param callback
+ * @returns {*}
+ */
+exports.service = function handleLambdaRequest(event, context, callback) {
+  if (event.source === 'aws.events') {
+    return callback(null, "Warmed up - it's cosy now")
+  }
+  // eslint-disable-next-line global-require
+  const service = require('./src/index')
+  // eslint-disable-next-line global-require
+  const awsServerlessExpress = require('aws-serverless-express')
+  const server = awsServerlessExpress.createServer(service)
+  awsServerlessExpress.proxy(server, event, context)
+}
--- src/app.js
+++ src/app.js
@@ -61,10 +61,3 @@ exports.startServer = (config, callback) => {
   return app.listen(port, callback)
 }
 
-if (require.main === module) {
-  const server = exports.startServer({}, () => {
-    const address = server.address()
-    winston.info('NODE_ENV: ' + process.env.NODE_ENV)
-    winston.info(`Listening on port ${address.port}! Send an HTTP POST to http://${address.address}:${address.port}/email/send for sending an email`)
-  })
-}
